1fdcd6ce18407154b641a07f91573feaa4b5fe59
laquisha-ayee <rayelaquisha@gmail.com>
2025-04-10 14:20:04 -0700

    phase3

--- src/components/FruitsIndex.jsx
+++ src/components/FruitsIndex.jsx
@@ -1,5 +1,18 @@
-function FruitsIndex() {
-  return null;
-}
+import { Link } from 'react-router-dom';
+
+const FruitsIndex = ({ fruits }) => {
+    return (
+        <div>
+            <h2>Fruits Index</h2>
+            <ul>
+                {fruits.map((fruit) => (
+                    <li key={fruit.id}>
+                        <Link to={`/fruits/${fruit.id}`}>{fruit.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default FruitsIndex;
