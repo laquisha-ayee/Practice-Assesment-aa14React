be722330c15c2615bf512ff8423555a0f7e635c5
laquisha-ayee <rayelaquisha@gmail.com>
2025-04-10 13:49:11 -0700

    phase 2

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,26 @@
-function Navigation() {
-  return null;
-}
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+  <ul>
+<li>
+<NavLink to="/">Home</NavLink>
+</li>
+<li>
+  <NavLink to="/fruits/new">Enter a Fruit</NavLink>
+  </li>
+<li>
+  <NavLink to="/fav-fruit">Favorite Fruit</NavLink>
+</li>
+<li>
+  <NavLink to= "/set-fav-fruit">Set Favorite Fruit</NavLink>
+
+</li>
+</ul>
+  </nav>
+
+  );
+};
 
 export default Navigation;
