c8b99b7d7b39367e8f7149e050c63f1bb4803803
laquisha-ayee <rayelaquisha@gmail.com>
2025-04-11 10:00:40 -0700

    phase4 done

--- src/components/FruitShow.jsx
+++ src/components/FruitShow.jsx
@@ -1,5 +1,21 @@
-function FruitShow() {
-  return null;
-}
+import { useParams } from 'react-router-dom';
+
+const FruitShow = ({ fruits }) => {
+    const { fruitId } = useParams();
+    const fruit = fruits.find(f => f.id === fruitId);
+
+    if (!fruit) {
+        return <h2>Fruit not found</h2>;
+    }
+
+    return (
+        <div>
+            <h2>{fruit.name}</h2>
+            <p>Color: {fruit.color}</p>
+            <p>Sweetness: {fruit.sweetness}</p>
+            <p>Seeds: {fruit.seeds === "yes" ? "Yes" : "no"}</p>
+        </div>
+    );
+};
 
 export default FruitShow;
